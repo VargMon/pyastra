c58b318aca6bb83c0caa328df072f612ab2733b3
estyler <estyler@a9130678-b50d-0410-94fe-af15eb06aa22>
2004-06-04 16:16:53 +0000

    Internal EEPROM added as an example.

---
+++ Pyastra/doc/examples/eeprom.py
@@ -0,0 +1,43 @@
+############################################################################
+# $Id$
+#
+# Description: Internal EEPROM driver. Pyastra project.
+# Author: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>
+#    
+# Copyright (c) 2004 Alex Ziranov.  All rights reserved.
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+############################################################################
+
+def EEPROM_write(EEPROM_write_addr, EEPROM_write_data):
+    EEDATA = EEPROM_write_data
+    EEADR = EEPROM_write_addr
+    EECON1[EEPGD] = 0
+    EECON1[WREN] = 1
+    INTCON[GIE] = 0
+    EECON2 = 0x55
+    EECON2 = 0xaa
+    EECON1[WR] = 1
+    INTCON[GIE] = 1
+    EECON1[WR] = 0
+
+def EEPROM_read(EEPROM_read_addr):
+    EEADR = EEPROM_read_addr
+    EECON1[EEPGD] = 0
+    EECON1[RD] = 1
+    return EEDATA
